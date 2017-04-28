dd29eb56cf819d38fe13505b037f4bb1faec0e31
Ron <tront1992@gmail.com>
2017-04-28 15:42:50 +0300

    aux updated

--- SPAssignment2/main.c
+++ SPAssignment2/main.c
@@ -8,7 +8,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "SPBufferset.h"
+#include "main_aux.h"
 
 int main() {
+	int arr[] = {1,3,2,0};
+	statusPrint(arr, 4, 200);
+	boardPrint(arr,4);
+
 	return 0;
 }
